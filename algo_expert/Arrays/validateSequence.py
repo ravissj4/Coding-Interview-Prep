1f24b77ba1105d5fa3d612977f77b6c21f040389
RAVI RANJAN <raviranjan@cse.iitb.ac.in>
2020-06-01 17:42:13 +0530

    debugged validateSequence.py

--- algo_expert/Arrays/validateSequence.py
+++ algo_expert/Arrays/validateSequence.py
@@ -1,14 +1,17 @@
 def isValidSubsequence(array, sequence):
-    j = 0
-    for i in range(len(sequence)):
+	i,j=0,0
+	matched_elements = 0
+	while i != len(sequence):
+		# print(f'i = {i} and j = {j}')
         if j == len(array):
             break
         if sequence[i] == array[j]:
             i += 1
             j += 1
+			matched_elements += 1
         else:
             j += 1
-    if j == len(array):
-        return False
-    else:
-        return True
+	# print(matched_elements)
+	# print(len(sequence))
+	return matched_elements == len(sequence)
+    
